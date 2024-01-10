3a78e09267b0363bf85200b3022685492ecfab6d
Nir Cohen <nircohen.biz@gmail.com>
2024-01-10 18:15:52 +0200

    Fix toggle like btn to change color

--- src/cmps/StoryIcons.jsx
+++ src/cmps/StoryIcons.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import { eventBus } from '../services/event-bus.service'
 import { storyService } from '../services/story.service.local'
 import { utilService } from '../services/util.service'
+import { userService } from '../services/user.service'
 
 export function StoryIcons({ story }) {
+    const [isLiked, setIsLiked] = useState(
+        story.likedBy.some((u) => u._id === userService.getLoggedinUser()._id)
+    )
+
     async function handleLike() {
         try {
-            await storyService.toggleLike(story)
-            eventBus.emit('toggleLike', story)
+            const updatedStory = await storyService.toggleLike(story)
+            setIsLiked(
+                updatedStory.likedBy.some(
+                    (u) => u._id === userService.getLoggedinUser()._id
+                )
+            )
+
+            eventBus.emit('toggleLike', updatedStory)
         } catch (err) {
             console.error('Cannot toggle like', err)
         }
@@ -14,15 +26,18 @@ export function StoryIcons({ story }) {
 
     let likeStr = utilService.createLikeStr(story.likedBy.length)
 
+    const iconLikeCls = `icon-img like ${isLiked ? 'liked' : ''}`
+    const toggleLikeTitleCls = `${isLiked ? 'Unlike' : 'Like'}`
     // debugger
     return (
         <>
             <div className="story-icons flex align-center">
                 <button
-                    className="icon-img like"
-                    title="Like/Unlike"
+                    className={iconLikeCls}
+                    title={toggleLikeTitleCls}
                     onClick={handleLike}
                 ></button>
+
                 <button className="icon-img comment" title="Comment"></button>
                 <button className="icon-img share" title="Share"></button>
                 <button className="icon-img save" title="Save"></button>
