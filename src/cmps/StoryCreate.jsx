19476bbbb18499eb53537244bfdba022269db864
Nir Cohen <nircohen.biz@gmail.com>
2024-01-10 02:49:09 +0200

    Add emoji picker for StoryCreate

--- src/cmps/StoryCreate.jsx
+++ src/cmps/StoryCreate.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-import { storyService } from '../services/story.service.local'
-import { utilService } from '../services/util.service'
+
 import { useDispatch, useSelector } from 'react-redux'
 import { getActionUpdateStory } from '../store/story.actions'
 import { useNavigate } from 'react-router'
+
+import EmojiPicker from 'emoji-picker-react'
+
+import { storyService } from '../services/story.service.local'
+import { utilService } from '../services/util.service'
+import Back from '../img/svg/back.svg'
+import { Modal } from './Modal'
+
 export function StoryCreate({ onCloseModal }) {
     const [image, setImage] = useState(null)
     const [previewUrl, setPreviewUrl] = useState('')
     const [text, setText] = useState('')
+    // const [showEmojiPicker, setShowEmojiPicker] = useState(false)
+    const [isEmojiModalOpen, setIsEmojiModalOpen] = useState(false)
     const user = useSelector((storeState) => storeState.userModule.user)
 
     const dispatch = useDispatch()
@@ -32,6 +41,8 @@ export function StoryCreate({ onCloseModal }) {
     async function handleSubmit(ev) {
         try {
             ev.preventDefault()
+            if (!text || !previewUrl) return
+
             let storyToAdd = storyService.getEmptyStory()
 
             storyToAdd.createdAt = Date.now()
@@ -68,21 +79,89 @@ export function StoryCreate({ onCloseModal }) {
         }
     }
 
+    function onBack() {
+        navigate('/')
+        onCloseModal()
+    }
+
+    const handleEmojiClick = (emojiObject, event) => {
+        console.log(
+            '🚀 ~ file: StoryCreate.jsx:89 ~ handleEmojiClick ~ emojiObject:',
+            emojiObject
+        )
+        if (emojiObject && emojiObject.emoji) {
+            setText((prevText) => prevText + emojiObject.emoji)
+            setIsEmojiModalOpen(false)
+        } else {
+            console.error('Emoji data not found')
+        }
+    }
+
+    const toggleEmojiPicker = () => {
+        setIsEmojiModalOpen(!isEmojiModalOpen)
+    }
+
+    const handleCloseEmojiModal = () => {
+        setIsEmojiModalOpen(false)
+    }
+
     return (
-        <div className="story-create flex column">
-            <h1>Create new post</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="file"
-                    onChange={handleFileChange}
-                    accept="image/*"
-                    required
+        <div className="story-create">
+            <header className="story-create-header flex ">
+                <img
+                    className="icon-img back"
+                    src={Back}
+                    alt="Back"
+                    title="Back"
+                    onClick={onBack}
                 />
+                <span className="create-post-title">Create new post</span>
+                <h1
+                    className="create-post-share"
+                    onClick={handleSubmit}
+                    title="Share"
+                >
+                    Share
+                </h1>
+            </header>
+            <form className="story-create-form flex " onSubmit={handleSubmit}>
+                {!previewUrl && (
+                    <input
+                        className="story-create-upload"
+                        type="file"
+                        onChange={handleFileChange}
+                        accept="image/*"
+                        required
+                    />
+                )}
                 {previewUrl && <img src={previewUrl} alt="Preview" />}
-                <textarea value={text} onChange={handleTextChange} required />
-                <button type="submit">Submit</button>
+                <div className="story-user-text-emoji ">
+                    <textarea
+                        className="story-create-textarea"
+                        value={text}
+                        placeholder="Write a caption..."
+                        onChange={handleTextChange}
+                        required
+                    />
+
+                    {isEmojiModalOpen && (
+                        <Modal
+                            isOpen={isEmojiModalOpen}
+                            onClose={handleCloseEmojiModal}
+                        >
+                            <EmojiPicker onEmojiClick={handleEmojiClick} />
+                        </Modal>
+                    )}
+                    <span
+                        className="toggle-emoji-picker"
+                        onClick={toggleEmojiPicker}
+                    >
+                        😊
+                    </span>
+                </div>
+
+                {/* <button type="submit">Submit</button> */}
             </form>
-            <p>Text: {text}</p> {/* Displaying the text for demonstration */}
         </div>
     )
 }
