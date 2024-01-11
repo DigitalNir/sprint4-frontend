789fed87a51ea924f759d63020b2d2e15bbb39eb
Nir Cohen <nircohen.biz@gmail.com>
2024-01-11 16:36:45 +0200

    Resume scroll in body and hide scroll in storydetail - comments

--- src/cmps/Modal.jsx
+++ src/cmps/Modal.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 export function Modal({ isOpen, children, onClose }) {
+    useEffect(() => {
+        if (isOpen) {
+            // Add the overflow hidden style to body when the modal is open
+            document.body.style.overflow = 'hidden'
+        }
+
+        return () => {
+            // Reset the overflow style on body when the modal is closed
+            document.body.style.overflow = ''
+        }
+    }, [isOpen])
+
     if (!isOpen) return null
 
     const handleBackdropClick = (e) => {
